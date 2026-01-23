2b1a2d8b42372b4de36de700399f108eb3dec0ed
QAnikolaysr <nikolaysr@gmail.com>
2026-01-23 14:29:29 +0300

    Gitignore is added

--- src/test/java/FirstLoginTest.java
+++ src/test/java/FirstLoginTest.java
@@ -4,23 +4,50 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
 public class FirstLoginTest {
     //открыть браузер
     //зайдем
     @Test
-    public void firstLogin() throws InterruptedException {
-
+    public void firstLogin()  {
 
         WebDriver browser = new ChromeDriver();
         browser.get("https://saucedemo.com/");
 
-        browser.findElement(By.id("user-name")).sendKeys(("standard_user"));
-        browser.findElement(By.id("user-name")).sendKeys((Keys.BACK_SPACE + "" + Keys.CONTROL + "A"));
         browser.findElement(By.id("user-name")).sendKeys("standard_user");
 
-        browser.findElement(By.xpath("//*[@data-test='password']")).sendKeys("secret_sauce");
+        browser.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
 
         browser.findElement(By.cssSelector("[name='login-button']")).click();
+
+        boolean titleIsDisplayed = browser.findElement(By.cssSelector("[data-test='title']")).isDisplayed();
+        assertTrue("Заголовок не виден", titleIsDisplayed);
+
+        String titleName = browser.findElement(By.cssSelector("[data-test='title']")).getText();
+        assertEquals(titleName, "Products", "Не верный заголовок");
+        browser.quit();
     }
+    @Test
+    public void incorrectLogin() {
+
+        WebDriver browser = new ChromeDriver();
+        browser.get("https://www.saucedemo.com/");
+
+        browser.findElement(By.id("user-name")).sendKeys("locked_out_user");
+        browser.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
+        browser.findElement(By.cssSelector("[name='login-button']")).click();
+
+        boolean errorIsDisplayed = browser.findElement(By.cssSelector("[data-test='error']")).isDisplayed();
+        assertTrue("Нет сообщения об ошибке", errorIsDisplayed);
+
+        assertEquals(browser.findElement(By.cssSelector("[data-test='error']")).getText(),
+                "Epic sadface: Sorry, this user has been locked out." ,
+                "Не верный текст сообщения об ошибке");
+
+        browser.quit();
+    }
+
 
 }
