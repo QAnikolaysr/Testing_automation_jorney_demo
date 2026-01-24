0f54bb18b791e979475480e7e9cdf27c0a62600f
QAnikolaysr <nikolaysr@gmail.com>
2026-01-24 17:01:25 +0300

    ifat-3 added

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,20 +5,25 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.time.Duration;
+
 public class BaseTest {
-    public WebDriver browser;
+    public WebDriver driver;
 
     @BeforeMethod
     public void setup(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments ("start-maximized");
+        //options.addArguments ("--window-size=1920,1080");
         //  options.addArguments ("headless");
-        browser = new ChromeDriver(options);
-        browser.get("https://saucedemo.com/");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        //browser.manage().window().maximize();
+        driver.get("https://saucedemo.com/");
     }
     @AfterMethod
     public void closeBrouser() {
-        browser.quit();
+        driver.quit();
     }
 }
 
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -8,22 +8,22 @@ import static org.testng.AssertJUnit.assertTrue;
 public class LoginTest extends BaseTest {
        @Test
     public void firstLogin(){
-        browser.findElement(By.id("user-name")).sendKeys("standard_user");
-        browser.findElement(By.cssSelector("[data-test=password]")).sendKeys("secret_sauce");
-        browser.findElement(By.cssSelector("[name='login-button']")).click();
-        boolean titleIsDisplayed = browser.findElement(By.cssSelector("[data-test='title']")).isDisplayed();
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.cssSelector("[data-test=password]")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("[name='login-button']")).click();
+        boolean titleIsDisplayed = driver.findElement(By.cssSelector("[data-test='title']")).isDisplayed();
         assertTrue("Заголовок не виден", titleIsDisplayed);
-        String titleName = browser.findElement(By.cssSelector("[data-test='title']")).getText();
+        String titleName = driver.findElement(By.cssSelector("[data-test='title']")).getText();
         assertEquals(titleName, "Products", "Не верный заголовок");
     }
     @Test
     public void incorrectLogin() {
-        browser.findElement(By.id("user-name")).sendKeys("locked_out_user");
-        browser.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
-        browser.findElement(By.cssSelector("[name='login-button']")).click();
-        boolean errorIsDisplayed = browser.findElement(By.cssSelector("[data-test='error']")).isDisplayed();
+        driver.findElement(By.id("user-name")).sendKeys("locked_out_user");
+        driver.findElement(By.cssSelector("[data-test='password']")).sendKeys("secret_sauce");
+        driver.findElement(By.cssSelector("[name='login-button']")).click();
+        boolean errorIsDisplayed = driver.findElement(By.cssSelector("[data-test='error']")).isDisplayed();
         assertTrue("Нет сообщения об ошибке", errorIsDisplayed);
-        assertEquals(browser.findElement(By.cssSelector("[data-test='error']")).getText(),
+        assertEquals(driver.findElement(By.cssSelector("[data-test='error']")).getText(),
                 "Epic sadface: Sorry, this user has been locked out.",
                 "Не верный текст сообщения об ошибке");
     }
