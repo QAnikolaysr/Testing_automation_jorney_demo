25a551ceb115eeea121f8a5562b457d8239f812f
QAnikolaysr <nikolaysr@gmail.com>
2026-02-11 18:11:43 +0300

    lombok addeded.

--- src/test/java/enums/TitleNaming.java
+++ src/test/java/enums/TitleNaming.java
@@ -1,24 +1,16 @@
 package enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum TitleNaming {
     PRODUCTS("Products", "Prod"),
     CART("Your Cart", "Cart"),
     CHECKOUT("Checkout: Your Information", "Your Inform");
+
+
     private final String displayName;
     private final String shortName;
-
-
-    TitleNaming(String displayName, String shortName) {
-        this.displayName = displayName;
-        this.shortName = shortName;
-    }
-
-    public String getDisplayName() {
-        return displayName;
-    }
-
-    public String getShortName() {
-        return shortName;
-    }
 }
-
